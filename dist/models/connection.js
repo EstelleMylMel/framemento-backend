828a34f136fe5ecd0fc81d463175146d1631203e
Your name <Your email>
2023-12-11 11:32:32 +0100

    convert to typescript

---
+++ dist/models/connection.js
@@ -0,0 +1,6 @@
+"use strict";
+const mongoose = require('mongoose');
+const connectionString = process.env.CONNECTION_STRING;
+mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
+    .then(() => console.log('Database connected'))
+    .catch(error => console.error(error));
---
+++ dist/models/userProfile.js
@@ -0,0 +1,13 @@
+"use strict";
+const mongoose = require('mongoose');
+const userProfileSchema = mongoose.Schema({
+    username: String,
+    profilePicture: String,
+    camera: [{ required: false, type: mongoose.Schema.Types.ObjectId, ref: 'cameras' }],
+    lenses: [{ required: false, type: mongoose.Schema.Types.ObjectId, ref: 'lenses' }],
+    framesList: [{ required: false, type: mongoose.Schema.Types.ObjectId, ref: 'frames' }],
+    rollsList: [{ required: false, type: mongoose.Schema.Types.ObjectId, ref: 'rolls' }],
+    favorites: [{ required: false, type: mongoose.Schema.Types.ObjectId, ref: 'frames' }],
+    followedUsers: [{ required: false, type: mongoose.Schema.Types.ObjectId, ref: 'usersProfiles' }]
+});
+const UserProfile = mongoose.model('usersProfiles', userProfileSchema);
