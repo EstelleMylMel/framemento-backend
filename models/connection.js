c9f7a6ade28f85432b5c6dcd8598f552a90e4eb2
Your name <Your email>
2023-12-11 10:31:05 +0100

    init db

---
+++ models/connection.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const connectionString = process.env.CONNECTION_STRING;
+
+mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
+ .then(() => console.log('Database connected'))
+
+  .catch(error => console.error(error));
