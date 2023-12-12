d094cc58ee2143b1203679dc6242a83456e7393a
Your name <Your email>
2023-12-12 15:01:42 +0100

    duno

--- dist/routes/users.js
+++ dist/routes/users.js
@@ -7,33 +7,34 @@ const UserProfile = require('../models/userProfiles');
 const { checkBody } = require('../modules/checkBody');
 const uid2 = require('uid2');
 const bcrypt = require('bcrypt');
-router.get('/test', (req, res) => {
-    res.json({ result: 'test' });
-});
 // route permettant l'inscription
 router.post('/signup', (req, res) => {
     if (!checkBody(req.body, ['email', 'username', 'password'])) {
         res.json({ result: false, error: 'Missing or empty fields' });
         return;
     }
+    const { email, username, password } = req.body;
+    // EQUIVALENT A :
+    // const email = req.body.email;
+    // const username = req.body.username;
+    // const password = req.body.password;
     // S'assurer que l'utilisateur n'existe pas déjà avec son mail
-    UserConnection.findOne({ email: req.body.email }).then((data) => {
+    UserConnection.findOne({ email }).then((data) => {
         // Si l'utilisateur n'existe pas, on crée son compte
         if (data === null) {
             // Hachage du mdp
-            const hash = bcrypt.hashSync(req.body.password, 10);
+            const hash = bcrypt.hashSync(password, 10);
             // 1ère étape : créer un userProfile
             const newUserProfile = new UserProfile({
-                username: req.body.username,
-                profilePicture: '' //AJOUTER UNE PHOTO PAR DEFAUT
+                username,
+                profilePicture: '../assets/image-profil.jpg'
             });
             newUserProfile.save().then((data) => {
-                console.log(data);
                 //Récupération de l'ID du UserProfile qui vient d'être créé
                 const userProfileID = data._id;
                 //2ème étape : créer un userConnection
                 const newUserConnection = new UserConnection({
-                    email: req.body.email,
+                    email,
                     password: hash,
                     token: uid2(32),
                     profile: userProfileID
@@ -54,8 +55,12 @@ router.post('/signin', (req, res) => {
     if (!checkBody(req.body, ['email', 'password'])) {
         res.json({ result: false, error: 'Missing or empty fields' });
     }
-    UserConnection.findOne({ email: req.body.email }).then((data) => {
-        if (data && bcrypt.compareSync(req.body.password, data.password)) {
+    const { email, password } = req.body;
+    // EQUIVALENT A :
+    //const email = req.body.email;
+    //const password = req.body.password;
+    UserConnection.findOne({ email }).then((data) => {
+        if (data && bcrypt.compareSync(password, data.password)) {
             res.json({ result: true, token: data.token });
         }
         else {
