496df695f64ccb0b7344f39c61e38a433eee4dce
Your name <Your email>
2023-12-15 18:05:57 +0100

    routes ajout et suppression lens et camera

--- routes/material.ts
+++ routes/material.ts
@@ -4,69 +4,173 @@ var router = express.Router();
 // IMPORT MODULES
 const Camera = require('../models/cameras');
 const Lens = require('../models/lenses');
+const UserProfile = require('../models/userProfiles');
+const { checkBody } = require('../modules/checkBody');
 
 // IMPORT TYPES
 import { Request, Response } from 'express';
 import { CameraType } from '../types/camera';
 import { LensType } from '../types/lens';
+import { UserProfileType } from '../types/userProfile';
 
 
 /// VOIR LA LISTE DES CAMERAS SUR MYMATERIEL ///
 
-router.get('/camera', (req: Request, res: Response) => {
-	Camera.find()
-    .then((data: CameraType[] | null) => {
-        if (data !== null) {
-            res.json({ result: true, cameras: data });
-        }
-		else {
-            res.json({ result: false })
-        }
-	});
+router.get('/camera/:id', (req: Request, res: Response) => {
+  const userId = req.params.id;
+
+  UserProfile.findById(userId)
+    .populate('cameras')
+    .then((userProfile: UserProfileType | null) => {
+      // Vérifiez si le profil utilisateur a été trouvé
+      if (!userProfile) {
+        return res.status(404).json({ result: false, message: 'User profile not found' });
+      }
+      // Renvoyez la liste des caméras associées à l'utilisateur
+      res.json({ result: true, cameras: userProfile.cameras });
+    })
+  })
+
+/// AJOUTER DES CAMERAS ///
+
+router.post('/camera', (req: Request, res: Response) => {
+  if (!checkBody(req.body, ['brand', 'model'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+
+  Camera.findOne({ brand: req.body.brand, model: req.body.model })
+    .then((existingCamera: CameraType | null) => {
+      if (existingCamera) {
+        res.json({ result: false, error: 'Camera already exists' });
+        return;
+      }
+
+      const newCamera = new Camera({
+        brand: req.body.brand,
+        model: req.body.model,
+      });
+
+      newCamera.save()
+        .then((savedCamera: CameraType) => {
+        res.json({ result: true, newCamera: savedCamera, id: savedCamera._id });
+      });
+    })
 });
 
 /// SUPPRIMER UNE CAMERA ///
 
 router.delete("/camera/:id", (req: Request, res: Response) => {
-    const cameraId = req.params.id;
-  
-    Camera.deleteOne({ _id: cameraId }).then((deletedCamera: any) => {
-        if (deletedCamera) {
-          res.json({ result: true, message: "Camera deleted successfully"})
-        } else {
-          res.json({ result: false, error: "Camera not found" });
-        }
+  const cameraId = req.params.id;
+
+  Camera.deleteOne({ _id: cameraId })
+    .then((deletedCamera: any) => {
+      console.log(deletedCamera)
+      if (deletedCamera.deletedCount > 0) {
+        return UserProfile.updateOne(
+          { cameras: cameraId },
+          { $pull: { cameras: cameraId } }
+        );
+      } else {
+        res.json({ result : false, error: "Camera not found" })
+      }
+    })
+    .then((result: any) => {
+      if (result.modifiedCount > 0) {
+        // Si au moins un document a été modifié, cela signifie que la caméra a été retirée avec succès
+        res.json({ result: true, message: "Camera deleted successfully" });
+        console.log(result)
+      } else {
+            // Si aucun document n'a été modifié, cela signifie que la caméra n'a pas été trouvée dans le profil utilisateur
+        res.json({ result: false, error: "Camera not found" });
+        console.log(result)
+      }
+    })
+    .catch((error: Error) => {
+      // Gérer les erreurs ici
+      console.error("Error deleting camera:", error);
+      res.status(500).json({ result: false, error: "Internal server error" });
     });
-      
-  });
+});
+
 
 /// VOIR LA LISTE DES OBJECTIFS SUR MYMATERIEL ///
 
-router.get('/lens', (req: Request, res: Response) => {
-	Lens.find()
-    .then((data: LensType[] | null) => {
-        if (data !== null) {
-            res.json({ result: true, lenses: data });
-        }
-		else {
-            res.json({ result: false })
-        }
-	});
+router.get('/lens/:id', (req: Request, res: Response) => {
+  const userId = req.params.id;
+
+  UserProfile.findById(userId)
+    .populate('lenses')
+    .then((userProfile: UserProfileType | null) => {
+      // Vérifiez si le profil utilisateur a été trouvé
+      if (!userProfile) {
+        return res.status(404).json({ result: false, message: 'User profile not found' });
+      }
+      // Renvoyez la liste des objectifs associées à l'utilisateur
+      res.json({ result: true, lenses: userProfile.lenses });
+    })
+  })
+
+/// AJOUTER UN OBJECTIF ///
+
+router.post('/lens', (req: Request, res: Response) => {
+  if (!checkBody(req.body, ['brand', 'model'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+
+  Lens.findOne({ brand: req.body.brand, model: req.body.model })
+    .then((existingLens: LensType | null) => {
+      if (existingLens) {
+        res.json({ result: false, error: 'Lens already exists' });
+        return;
+      }
+
+      const newLens = new Lens({
+        brand: req.body.brand,
+        model: req.body.model,
+      });
+
+      newLens.save().then((savedLens: LensType) => {
+        res.json({ result: true, newLens: savedLens, id: savedLens._id });
+      });
+    })
 });
 
 /// SUPPRIMER UN OBJECTIF ///
 
 router.delete("/lens/:id", (req: Request, res: Response) => {
-    const lensId = req.params.id;
-  
-    Lens.deleteOne({ _id: lensId }).then((deletedLens: any) => {
-        if (deletedLens) {
-          res.json({ result: true, message: "Lens deleted successfully"})
-        } else {
-          res.json({ result: false, error: "Lens not found" });
-        }
-    });
-      
-  });
+  const lensId = req.params.id;
 
+  Lens.deleteOne({ _id: lensId })
+    .then((deletedLens: any) => {
+      console.log(deletedLens)
+      if (deletedLens.deletedCount > 0) {
+        return UserProfile.updateOne(
+          { lenses: lensId },
+          { $pull: { lenses: lensId } }
+        );
+      } else {
+        res.json({ result : false, error: "Lens not found" })
+      }
+    })
+    .then((result: any) => {
+      if (result.modifiedCount > 0) {
+        // Si au moins un document a été modifié, cela signifie que la caméra a été retirée avec succès
+        res.json({ result: true, message: "Lens deleted successfully" });
+        console.log(result)
+      } else {
+            // Si aucun document n'a été modifié, cela signifie que la caméra n'a pas été trouvée dans le profil utilisateur
+        res.json({ result: false, error: "Lens not found" });
+        console.log(result)
+      }
+    })
+    .catch((error: Error) => {
+      // Gérer les erreurs ici
+      console.error("Error deleting camera:", error);
+      res.status(500).json({ result: false, error: "Internal server error" });
+    });
+});
+
+  
 module.exports = router;
